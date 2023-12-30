0855379208ae684db46abec1e93a81b9c8a3d69c
Niraj <developer.nirajchaurasiya@gmail.com>
2023-12-30 20:20:49 +0545

    fix bugs

--- auth/getUser/getUser.js
+++ auth/getUser/getUser.js
@@ -1,10 +1,13 @@
 const Router = require("express").Router();
 const UserModel = require("../../Models/UserModel/UserModel");
 const mongoose = require("mongoose");
+
 Router.get("/:identifier", async (req, res) => {
   try {
-    const { identifier } = req.params;
-    console.log(identifier);
+    let { identifier } = req.params;
+    identifier = identifier && identifier.trim(); // Remove leading and trailing spaces
+    console.log("Identifier:", identifier);
+
     // Check if the identifier is a valid ObjectId
     const isObjectId = mongoose.Types.ObjectId.isValid(identifier);
 
@@ -18,6 +21,7 @@ Router.get("/:identifier", async (req, res) => {
     }
 
     if (getUser) {
+      console.log("User found:", getUser);
       return res.status(200).send({
         status: 1,
         msg: "User retrieved successfully",
@@ -25,12 +29,14 @@ Router.get("/:identifier", async (req, res) => {
       });
     }
 
-    return res.status(200).send({
+    console.log("User not found for identifier:", identifier);
+    return res.status(404).send({
       status: 2,
       msg: "User with that username or _id doesn't exist",
       data: null,
     });
   } catch (error) {
+    console.error("Error:", error.message);
     res.status(500).send({
       status: 4,
       msg: "Server error. Please try again later.",
